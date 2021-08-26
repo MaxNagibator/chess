74bf0b81dfc1037a7fc6bf12346c025db7fbf391
Maksim Gritsina <bob217@mail.ru>
2021-08-26 21:01:48 +0700

    сайт для игры в шахматы
    - наброски для "поиска игры"
    - наброски для обёртки http запросов
    
    t: 75m

---
+++ src/Bg.Chess.Web/Controllers/ChessController.cs
@@ -0,0 +1,100 @@
+﻿using Bg.Chess.Domain;
+using Bg.Chess.Web.Models;
+using Bg.Chess.Web.Repo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bg.Chess.Web.Controllers
+{
+    public class ChessController : Controller
+    {
+        private readonly ILogger<ChessController> _logger;
+
+        public ChessController(ILogger<HomeController> logger)
+        {
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        private static string searchId { get; set; }
+        private static int currentCheck { get; set; }
+
+        public JsonResult SearchGame()
+        {
+            // todo создать GameHolder/SearchQueue
+            searchId = Guid.NewGuid().ToString("N");
+            game = null;
+            currentCheck = 0;
+            return Json(new { searchId = searchId });
+        }
+
+        public JsonResult CheckSearch(string searchId)
+        {
+            if (currentCheck > 5)
+            {
+                var gameId = Guid.NewGuid().ToString("N");
+                game = new Game();
+                game.Init();
+                return Json(new { gameId = gameId });
+            }
+            else
+            {
+                currentCheck++;
+            }
+
+            return Json(new { waitpliz = true });
+        }
+
+        private static Game game;
+        private static Game Game
+        {
+            get
+            {
+                if (game == null)
+                {
+                    game = new Game();
+                    game.Init();
+                }
+                return game;
+            }
+        }
+
+        [HttpGet]
+        [Route("Chess/Game/{gameId}")]
+        public JsonResult GetGame(string gameId)
+        {
+            var notation = Game.GetForsythEdwardsNotation();
+            var moves = Game.AvailableMove();
+
+            return Json(new { Notation = notation, AvailableMoves = moves });
+        }
+
+        //[HttpGet]
+        //public JsonResult GetField()
+        //{
+        //    var player = _playerRepo.GetPlayer();
+        //    var notation = Game.GetForsythEdwardsNotation();
+        //    var moves = Game.AvailableMove();
+        //    // todo сделать dto
+        //    return Json(new { Notation = notation, AvailableMoves = moves, Player = player.Name });
+        //}
+
+        //[HttpPost]
+        //public JsonResult Move(int fromX, int fromY, int toX, int toY)
+        //{
+        //    Game.Move(Game.StepSide, fromX, fromY, toX, toY);
+        //    var notation = Game.GetForsythEdwardsNotation();
+        //    var moves = Game.AvailableMove();
+        //    return Json(new { Notation = notation, AvailableMoves = moves });
+        //}
+    }
+}
--- src/Bg.Chess.Web/wwwroot/js/site.js
+++ src/Bg.Chess.Web/wwwroot/js/site.js
@@ -2,3 +2,49 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function SendRequest(options) {
+    let _this = this;
+    let defaultOptions = {
+        method: 'POST',
+    }
+
+    _this.options = Object.assign({}, defaultOptions, options);
+
+    _this.Send = function () {
+        let bodyString = '';
+        let first = true;
+
+        for (var prop in _this.options.body) {
+            if (first == false) {
+                bodyString += "&";
+            }
+            bodyString += prop + "=" + _this.options.body[prop];
+            first = false;
+        }
+
+        let xhr = new XMLHttpRequest();
+        xhr.open(_this.options.method, _this.options.url, true);
+        xhr.setRequestHeader('Content-Type', "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (this.readyState != 4) {
+                return;
+            }
+            if (this.status == 200) {
+                if (_this.options.success) {
+                    _this.options.success(this);
+                }
+            } else {
+                if (_this.options.error) {
+                    _this.options.error(this);
+                }
+            }
+            if (_this.options.always) {
+                _this.options.always(this);
+            }
+        };
+        xhr.send(bodyString);
+    }
+    _this.Send();
+}
