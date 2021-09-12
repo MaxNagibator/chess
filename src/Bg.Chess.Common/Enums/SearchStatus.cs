1a8c97362f0c66251d9d00b3e5eed4db4e304baf
Maksim Gritsina <bob217@mail.ru>
2021-09-12 22:25:05 +0700

    сайт для игры в шахматы
    - отправил логику шахматишек v.0.1.0 в nuget.org
    - реструктуризация проэкта
    
    t: 40m

--- src/Bg.Chess.Common/Enums/SearchStatus.cs
+++ src/Bg.Chess.Common/Enums/SearchStatus.cs
@@ -1,12 +1,33 @@
 ﻿namespace Bg.Chess.Common.Enums
 {
-    //todo summary
+    /// <summary>
+    /// Статус поиска.
+    /// </summary>
     public enum SearchStatus
     {
+        /// <summary>
+        /// Не существует.
+        /// </summary>
         NotFound = 0,
+
+        /// <summary>
+        /// В процессе.
+        /// </summary>
         InProcess = 1,
+
+        /// <summary>
+        /// Ожидается подтверждение.
+        /// </summary>
         NeedConfirm = 2,
+
+        /// <summary>
+        /// Ожидается подтверждение противника.
+        /// </summary>
         NeedConfirmOpponent = 3,
+
+        /// <summary>
+        /// Окончен.
+        /// </summary>
         Finish = 4,
     }
 }
